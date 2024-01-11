7b4ff25dccd18732acfdaf72dabde721792da8b5
Blake Gentry <blakesgentry@gmail.com>
2024-01-11 09:35:22 -0600

    add Client to work context, helpers to extract it
    This change adds the `*Client` to an unexported context key within the
    work context when the Client is started, making it available to the
    context for every job that's worked by that Client.
    
    Additionally, two new helper functions are added to retrieve the Client
    from the context within a Worker: `ClientFromContext` and
    `ClientFromContextSafely`. The only difference between these is that the
    first variant panics if the client isn't found in the context, whereas
    the latter returns an error. However if these are used on the context
    provided to a Worker's `Work()` method (or one derived from it) they
    should never panic/error.

---
+++ context.go
@@ -0,0 +1,44 @@
+package river
+
+import (
+	"context"
+	"errors"
+)
+
+type ctxKey int
+
+const (
+	ctxKeyClient ctxKey = iota
+)
+
+func withClient[TTx any](ctx context.Context, client *Client[TTx]) context.Context {
+	return context.WithValue(ctx, ctxKeyClient, client)
+}
+
+// ClientFromContext returns the Client from the context. This function can
+// only be used within a Worker's Work() method because that is the only place
+// River sets the Client on the context.
+//
+// It panics if the context does not contain a Client, which will never happen
+// from the context provided to a Worker's Work() method.
+func ClientFromContext[TTx any](ctx context.Context) *Client[TTx] {
+	client, err := ClientFromContextSafely[TTx](ctx)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+// ClientFromContext returns the Client from the context. This function can
+// only be used within a Worker's Work() method because that is the only place
+// River sets the Client on the context.
+//
+// It returns an error if the context does not contain a Client, which will
+// never happen from the context provided to a Worker's Work() method.
+func ClientFromContextSafely[TTx any](ctx context.Context) (*Client[TTx], error) {
+	client, exists := ctx.Value(ctxKeyClient).(*Client[TTx])
+	if !exists || client == nil {
+		return nil, errors.New("river: client not found in context, can only be used in a Worker")
+	}
+	return client, nil
+}
